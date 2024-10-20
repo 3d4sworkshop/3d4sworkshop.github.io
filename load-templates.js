1ebb4c34eea361c5d170b99d45c145d47de7f91e
Wuyang Chen <chenwydj@gmail.com>
2024-10-20 14:08:20 -0700

    bug fixed for js

--- load-templates.js
+++ load-templates.js
@@ -11,6 +11,7 @@ function loadHTML(elementId, filePath) {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+    window.scrollTo(0, 0);  // Ensure scroll to top on page load
     loadHTML('header', 'header.html');
     loadHTML('navbar', 'navbar.html');
     loadHTML('footer', 'footer.html');
